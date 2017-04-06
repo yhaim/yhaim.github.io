6ecfd1216b6d3c0e16d0beab7572181a5a06299c
yhaim <2861135061@qq.com>
2017-04-06 17:11:21 +0800

    文章成功侧边失败

--- js/main.js
+++ js/main.js
@@ -1,2 +1,41 @@
-// JavaScript Document
-console.log('lalla');
+
+	;$(function()
+	{
+		'use strict';
+		
+		var sidebar=$('.sidebar'),
+			mask=$('.mask'),
+			backButton=$('.back-to-top'),
+			sidebar_trigger=$('#sidebar_trigger');
+			
+			function showsidebar()
+			{
+				mask.fadeIn();
+				sidebar.css('right',0);
+			}
+			
+			function hidsidebar()
+			{
+				mask.fadeout();
+				sidebar.css('right',-sidebar.width());
+			}
+			
+		sidebar_trigger.on('click',showsidebar)
+		mask.on('click',hidsidebar)
+		backButton.on('click',function()
+		{
+			$('html,body').animate({
+					scrollTop:0
+				},800)
+		})
+		
+			$(window).on('scroll',function(){
+				if($window).scrollTop()>$(window).height())
+					backButton.fadeIn(); 
+				else
+					backButton.fadeOut();
+			})
+			
+			$(windows).trigger('scroll');
+		
+	})
