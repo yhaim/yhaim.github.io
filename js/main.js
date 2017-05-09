24a609193d271158aabb24a7ea830db2f4413fd8
yhaim <2861135061@qq.com>
2017-05-09 10:11:34 +0800

    article

--- js/main.js
+++ js/main.js
@@ -1,15 +1,41 @@
-	
+
 	;$(function()
 	{
 		'use strict';
 		
-		var sidebar=$('#sidebar'),
-			mask=$('.mask'),	
+		var sidebar=$('.sidebar'),
+			mask=$('.mask'),
+			backButton=$('.back-to-top'),
 			sidebar_trigger=$('#sidebar_trigger');
 			
-			sidebar_trigger.on('click',function()
+			function showsidebar()
 			{
-					mask.fadeIn();
-				})
+				mask.fadeIn();
+				sidebar.css('right',0);
+			}
+			
+			function hidsidebar()
+			{
+				mask.fadeout();
+				sidebar.css('right',-sidebar.width());
+			}
+			
+		sidebar_trigger.on('click',showsidebar)
+		mask.on('click',hidsidebar)
+		backButton.on('click',function()
+		{
+			$('html,body').animate({
+					scrollTop:0
+				},800)
+		})
+		
+			$(window).on('scroll',function(){
+				if($window).scrollTop()>$(window).height())
+					backButton.fadeIn(); 
+				else
+					backButton.fadeOut();
+			})
+			
+			$(windows).trigger('scroll');
+		
 	})
-	
